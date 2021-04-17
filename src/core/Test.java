180868dbeddcba0f08ce9cc9761948876f8b22d8
koh0012 <77301335+koh0012@users.noreply.github.com>
2021-04-17 00:52:21 -0500

    Delete src directory

--- src/core/Test.java
+++
@@ -1,8 +0,0 @@
-package core;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-public class Test {
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		Chatbot chatbot = new Chatbot();
-	}
-}
